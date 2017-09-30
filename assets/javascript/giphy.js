4115a6894497cdeda2ffc4a704bb60ecc4da8751
unknown <brandon.geeman.lee@gmail.com>
2017-09-30 16:25:58 -0700

    Buttons are rendering correctly

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,29 @@
+ $(document).ready(function(){
+
+
+  
+  var topics = ["Call of Duty", "Resident Evil", "Halo", "Metal Gear Solid", "Overwatch"];
+
+  function renderGameBtn() {
+
+    $("#gamebtn").empty();
+
+    for(var i = 0; i < topics.length; i++) {
+      
+      var newgbtn = $("<button type=button>");
+      // newgbtn.addClass("vGame");
+      // newgbtn.attr("data-name", topics[i]);
+      
+      //This will add the physical button to the page.
+      newgbtn.text(topics[i]);
+      $("#gamebtn").append(newgbtn);
+    }
+  }
+  $("#addGame").on("click", function(event) {
+    event.preventDefault();
+    var game = $("#game-input").val().trim();
+    topics.push(game);
+    renderGameBtn();
+  })
+  renderGameBtn();
+});
