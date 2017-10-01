4520fa7c22619dfba17fd2a1edd49cf5b054d4d9
unknown <brandon.geeman.lee@gmail.com>
2017-09-30 22:54:03 -0700

    Everything is functioning properly before applying static and animate attributes

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -4,36 +4,12 @@ $(document).ready(function(){
 
     
 
-  function renderGameBtn() {
-
-    $("#gamebtn").empty();
-
-    for(var i = 0; i < topics.length; i++) {
-      
-      var newgbtn = $("<button type=button class=btn btn-primary>");
-      newgbtn.addClass("vGame");
-      newgbtn.attr("data-game", topics[i]);
-      
-      //This will add the physical button to the page.
-      newgbtn.text(topics[i]);
-      $("#gamebtn").append(newgbtn);
-    }
-
-      $("#addGame").on("click", function(event) {
-        event.preventDefault();
-        var game = $("#game-input").val().trim();
-        topics.push(game);
-        renderGameBtn();
-      })
-  }
-
-    renderGameBtn();
-    
-    $("button").on("click", function(){
-      var gInput = $(this).attr("data-game");
-          
-          console.log(this);
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gInput + "&api_key=dc6zaTOxFJmzC&limit=10";
+function showgame() {
+      $("button").on("click", function(){
+        
+        var gInput = $(this).attr("data-game");
+        console.log(this);
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gInput + "&api_key=dc6zaTOxFJmzC&limit=10";
 
           $.ajax({
             url:queryURL,
@@ -57,5 +33,33 @@ $(document).ready(function(){
               }
             }
           })
-    })
+      })
+    }
+
+  function renderGameBtn() {
+
+    $("#gamebtn").empty();
+
+    for(var i = 0; i < topics.length; i++) {
+      
+      var newgbtn = $("<button type=button class=btn btn-primary>");
+      // newgbtn.addClass("vGame");
+      newgbtn.attr("data-game", topics[i]);
+      
+      //This will add the physical button to the page.
+      newgbtn.text(topics[i]);
+      $("#gamebtn").append(newgbtn);
+      showgame();
+    }
+  }
+
+      $("#addGame").on("click", function(event) {
+        event.preventDefault();
+        var game = $("#game-input").val().trim();
+        topics.push(game);
+        renderGameBtn();
+        showgame();
+      })
+    // showgame();
+    renderGameBtn();
 });
